712dbfced701afc329358d17a7e45fb7079ace82
benstrydom <benstrydom@omnea.co.za>
2024-01-31 15:46:36 +0200

    Add build scripts to ch11.
    Open with VSCode and use build scripts to compile and run after downloading the dependencies jars from https://search.maven.org/search?q=jackson-core (as described in chapter 11 of the Apress book) into the lib folder.

---
+++ ch11/JacksonDemo/v3/run.sh
@@ -0,0 +1,2 @@
+javac -cp ../lib/jackson-core-2.16.1.jar JacksonDemo.java
+java -cp ../lib/jackson-core-2.16.1.jar:. JacksonDemo
---
+++ ch11/JacksonDemo/v4/run.sh
@@ -0,0 +1,3 @@
+javac -cp ../lib/jackson-core-2.16.1.jar:../lib/jackson-databind-2.16.1.jar:../lib/jackson-annotations-2.16.1.jar JacksonDemo.java
+# javac -Xlint:deprecation -cp ../lib/jackson-core-2.16.1.jar:../lib/jackson-databind-2.16.1.jar:../lib/jackson-annotations-2.16.1.jar JacksonDemo.java
+java -cp ../lib/jackson-core-2.16.1.jar:../lib/jackson-databind-2.16.1.jar:../lib/jackson-annotations-2.16.1.jar:. JacksonDemo
